9a45a8bb51fcaa85a799e52e1cafb474b48e6d9f
Jay Hernandez <jeric614@yahoo.com>
2021-11-30 10:26:48 +0900

    anagram w/object map

---
+++ frequencyCounter/anagram.js
@@ -0,0 +1,35 @@
+//Anagrams
+//validAnagram('abc','cba') //true
+//validAnagram('','') //true
+//validAnagram('aaz', 'zza') //false
+
+//str.split('').reverse().join('') this is for string reversal
+
+function validAnagram(str1, str2) {
+  let strObj1 = {};
+  let strObj2 = {};
+
+  for (let key of str1) {
+    strObj1[key] = (strObj1[key] || 0) + 1;
+  }
+
+  for (let key of str2) {
+    strObj2[key] = (strObj2[key] || 0) + 1;
+  }
+
+  for (let key in strObj1) {
+    if (strObj1[char] !== strObj2[char]) {
+      return false;
+    }
+    return true;
+  }
+}
+
+validAnagram('hello', 'lolhe'); //true
+// validAnagram('aaz', 'zza'); //false
+// validAnagram('anagram', 'nagaram'); //true
+// validAnagram('rat', 'car'); //false
+// validAnagram('awesome', 'awesom'); //false
+// validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana'); //false
+// validAnagram('qwerty', 'qeywrt'); //true
+// validAnagram('texttwisttime', 'timetwisttext'); //true
---
+++ frequencyCounter/index.js
@@ -0,0 +1,44 @@
+//second array contains the square value of the first array
+//[1,2,3], [1,4,9] true
+
+//bad O(n)^2; avoid nested loop bad juju
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+same([1, 2, 3], [1, 4, 9]);
+
+//O(n)
+function same2(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+same2([1, 2, 3], [1, 4, 9]);

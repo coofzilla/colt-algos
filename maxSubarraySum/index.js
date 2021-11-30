ff15d2bea00c36c5998c480f85ae47966bd88bfa
Jay Hernandez <jeric614@yahoo.com>
2021-11-30 17:42:37 +0900

    sliding window max subarray

---
+++ maxSubarraySum/index.js
@@ -0,0 +1,24 @@
+const array = [5, 5, 5, 5, 5, 1, 1, 12, 1];
+
+//sliding window approach
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  if (arr.length < num) return null;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  console.log(maxSum);
+  return maxSum;
+}
+
+maxSubarraySum(array, 2);

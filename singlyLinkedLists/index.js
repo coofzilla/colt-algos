18fedbabb671c3be900d577fc1f6827cb0bb2343
Jay Hernandez <jeric614@yahoo.com>
2021-12-09 12:13:32 +0900

    pop f/linked list

---
+++ singlyLinkedLists/index.js
@@ -0,0 +1,70 @@
+//data structure contains head, tail, length
+//linked lists consist of nodes, each node has value and pointer
+//points to another node or null (like a train car)
+//dont have indices
+
+//arrays
+//indexed in order
+//insertion and deletion expensive
+//can quickly access specific index
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    //returning this allows to chain on other methods f/this instance
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push('Hello');
+list.push('Goodbye');
+list.push('!');
+
+list.pop();

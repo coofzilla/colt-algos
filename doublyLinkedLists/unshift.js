791c70facf63748c02316cbdd57b3decfb5250ec
Jay Hernandez <jeric614@yahoo.com>
2021-12-21 15:56:41 +0900

    unshift practice DLL

---
+++ doublyLinkedLists/unshift.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const node = new Node(val);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+  unshift(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new DoublyLinkedList();
+
+list.push(1);
+list.push(2);
+list.push(3);
+
+console.log(list);

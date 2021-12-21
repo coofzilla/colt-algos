a2084e05557c76e5d5fdfb606f9e488d7d30df4a
Jay Hernandez <jeric614@yahoo.com>
2021-12-21 12:04:33 +0900

    push practice with DLL

---
+++ doublyLinkedLists/push.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    //returning length to match w/array push return
+    return this.length;
+  }
+}
+
+const list = new DoublyLinkedList();
+
+list.push(1);
+list.push(2);
+console.log(list.push(3));

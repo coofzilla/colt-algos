c5a7fef56f37e2477e44d42268117dd90f237e2f
Jay Hernandez <jeric614@yahoo.com>
2021-12-26 16:08:14 +0900

    find f/tree

--- trees/index.js
+++ trees/index.js
@@ -38,6 +38,24 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(data) {
+    if (!this.root) return undefined;
+    if (this.root.data === data) return true;
+    let current = this.root;
+    while (true) {
+      if (data < current.data) {
+        if (!current.left) return false;
+        current = current.left;
+        if (current.data === data) return true;
+      }
+      if (data > current.data) {
+        if (!current.right) return false;
+        current = current.right;
+        if (current.data === data) return true;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -47,4 +65,5 @@ tree.insert(4);
 tree.insert(2);
 tree.insert(11);
 tree.insert(10);
+
 console.log(tree);

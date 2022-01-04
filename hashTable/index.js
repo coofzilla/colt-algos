b5b75836ddba0eb09fe18fc590537afdbe59b1fb
Jay Hernandez <jeric614@yahoo.com>
2022-01-04 11:38:54 +0900

    set f/hash table

--- hashTable/index.js
+++ hashTable/index.js
@@ -1,13 +1,28 @@
-function hash(key, arrLen) {
-  let total = 0;
-  //prime = dividied by itself and 1 (without remainder)
-  let prime = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * prime + value) % arrLen;
+class HashTable {
+  //default 53 if not include size
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+
+  hash(key) {
+    let total = 0;
+    //prime = dividied by itself and 1 (without remainder)
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  //separate chaining sharing arr(nested)
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
 }
 
-hash('hello', 5);
+const table = new HashTable();

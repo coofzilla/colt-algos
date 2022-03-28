7a8465d50ed15222cfb21437d16cd9e8c2a1c04a
coofzilla <jeric614@yahoo.com>
2022-03-28 14:38:36 -0400

    concat 2

---
+++ strings/getLengthOfWord.js
@@ -0,0 +1,7 @@
+var output = getLengthOfWord("some");
+console.log(output); // --> 4
+
+function getLengthOfWord(word) {
+  return word.length;
+}
+
---
+++ strings/getLengthofTwoWords.js
@@ -0,0 +1,6 @@
+var output = getLengthOfTwoWords("some", "words");
+console.log(output); // --> 9
+
+function getLengthOfTwoWords(word1, word2) {
+  return word1.length + word2.length;
+}

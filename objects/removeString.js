ad9c00f825b2b046325cb99cf6285869f62473de
coofzilla <jeric614@yahoo.com>
2022-03-29 16:34:16 -0400

    remove string

---
+++ objects/removeString.js
@@ -0,0 +1,15 @@
+function removeStringValues(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+var obj = {
+  name: "Sam",
+  age: 20,
+};
+removeStringValues(obj);
+console.log(obj); // { age: 20 }

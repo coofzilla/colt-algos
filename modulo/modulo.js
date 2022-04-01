900c99e0a486e0f53a0167f32b7f9f9545b3bf83
coofzilla <jeric614@yahoo.com>
2022-04-01 11:11:36 -0400

    more correct modulo

--- modulo/modulo.js
+++ modulo/modulo.js
@@ -1,11 +1,33 @@
+// function modulo(num1, num2) {
+//   const quotient = parseInt(num1 / num2);
+//   const product = quotient * num2;
+//   const remainder = num1 - product;
+//   if (Math.sign(num1) === -1) {
+//     return -remainder;
+//   }
+//   return remainder;
+// }
+
 function modulo(num1, num2) {
-  const quotient = parseInt(num1 / num2);
-  const product = quotient * num2;
-  const remainder = num1 - product;
-  if (Math.sign(num1) === -1) {
-    return -remainder;
+  if (num1 === 0) return 0;
+  if (num2 === 0) return NaN;
+  if (isNaN(num1) || isNaN(num2)) return NaN;
+
+  //create bool flag
+  let resultIsPositive = true;
+  if (num1 < 0) resultIsPositive = false;
+  //make positive versions of num1 and num2
+  num1 = Math.abs(num1);
+  num2 = Math.abs(num2);
+
+  while (num1 >= num2) {
+    num1 = num1 - num2;
   }
-  return remainder;
+  //check result positive
+  if (resultIsPositive) return num1;
+
+  //if negative return w/negative
+  return -num1;
 }
 
 var output = modulo(25, 4);

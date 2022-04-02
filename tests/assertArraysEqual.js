a644faa3a77868cd94ea0eef74c9e4f916eb60d3
coofzilla <jeric614@yahoo.com>
2022-04-02 05:51:04 -0400

    assert array equal

---
+++ tests/assertArraysEqual.js
@@ -0,0 +1,22 @@
+var expected = ["b", "r", "o", "k", "e", "n"];
+var actual = "broken".split("");
+assertArraysEqual(actual, expected, "splits string into array of characters");
+// console output:
+// passed
+
+// var expected = [1, 1, 2, 3, 5, 8, 13];
+// var actual = generateFirstNFibonaccis(7);
+// assertArraysEqual(actual, expected, "generates first 7 Fibonacci numbers");
+// console output:
+// FAILED [generates first 7 Fibonacci numbers] Expected "1, 1, 2, 3, 5, 8, 13", but got "2, 3, 5, 8, 13, 21, 34"
+
+function assertArraysEqual(actual, expected, testName) {
+  if (actual.length !== expected.length) return console.log("failed");
+  for (let i = 0; i < expected; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`FAILED [${testName}] Expected ${expected}`);
+    }
+  }
+  console.log("passed");
+  return true;
+}

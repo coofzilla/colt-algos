b258543121f4d28f684842b5150d9d3f8b665176
coofzilla <jeric614@yahoo.com>
2022-04-04 14:50:46 +0900

    reduce w/odd even count

---
+++ reduce/oddEvenCounts.js
@@ -0,0 +1,17 @@
+const numbers = [5, 3, 8, 6, 9, 1, 0, 2, 2];
+const oddEvenCounts = numbers.reduce(
+  function (counts, number, index, array) {
+    function isOdd(number) {
+      return number % 2 !== 0;
+    }
+    if (isOdd(number)) {
+      counts.odd++;
+    } else {
+      counts.even++;
+    }
+    return counts;
+  },
+  { odd: 0, even: 0 }
+);
+
+console.log(oddEvenCounts);

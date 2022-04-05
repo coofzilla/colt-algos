485dddcdc5cf7d51f849befb595bb9fae5e81d44
coofzilla <jeric614@yahoo.com>
2022-04-05 11:24:08 +0900

    render inventory flat list

---
+++ fashionInventory/renderInventory.js
@@ -0,0 +1,31 @@
+const currentInventory = [
+  {
+    name: "Brunello Cucinelli",
+    shoes: [
+      { name: "tasselled black low-top lace-up", price: 1000 },
+      { name: "tasselled green low-top lace-up", price: 1100 },
+      { name: "plain beige suede moccasin", price: 950 },
+      { name: "plain olive suede moccasin", price: 1050 },
+    ],
+  },
+  {
+    name: "Gucci",
+    shoes: [
+      { name: "red leather laced sneakers", price: 800 },
+      { name: "black leather laced sneakers", price: 900 },
+    ],
+  },
+];
+
+function renderInventory(inventory) {
+  let flatList = ``;
+  for (let obj of inventory) {
+    for (let shoe of obj.shoes) {
+      flatList += `${obj.name}, ${shoe.name}, ${shoe.price}\n`;
+    }
+  }
+  return flatList;
+}
+
+//Create helper functions if needed
+renderInventory(currentInventory);

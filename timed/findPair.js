7a0ba697a22289f0392bf11c0de13e136d93fb4f
coofzilla <jeric614@yahoo.com>
2022-04-09 13:22:03 +0900

    find pair

---
+++ timed/findPair.js
@@ -0,0 +1,18 @@
+// Given a list of non-negative integers and a target sum, find a pair of numbers that sums to the target sum.
+
+// Example:
+
+const pair = findPairForSum([3, 34, 4, 12, 5, 2], 9);
+console.log(pair); // --> [4, 5]
+
+function findPairForSum(arr, target) {
+  let pair = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        pair.push(arr[i], arr[j]);
+      }
+    }
+  }
+  return pair;
+}

12e741550b8e8428f6ddc8433576567cc8473880
coofzilla <jeric614@yahoo.com>
2022-04-10 13:59:17 +0900

    pair practice

--- timed/findPair.js
+++ timed/findPair.js
@@ -16,3 +16,17 @@ function findPairForSum(arr, target) {
   }
   return pair;
 }
+
+const result = pairSum([3, 34, 4, 12, 5, 2], 9);
+
+function pairSum(arr, target) {
+  const pair = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) pair.push(arr[i], arr[j]);
+    }
+  }
+  if (!pair.length) return null;
+  return pair;
+}

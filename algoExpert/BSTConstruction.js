e88975783bbc0344e5976c0d6159143ad3af636d
coofzilla <jeric614@yahoo.com>
2022-04-16 17:14:30 +0900

    skeleton f/BSTConstruction

---
+++ algoExpert/BSTConstruction.js
@@ -0,0 +1,17 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    return this;
+  }
+
+  contains(value) {}
+
+  remove(value) {
+    return this;
+  }
+}
